cb19af20c4c0a153d9c445fa60fe3becba4744e8
bellsilver7 <bellsilver7@gmail.com>
2022-07-29 07:30:22 +0900

    사용자 비디오 및 음소거, 카메라 스위치 버튼 생성

---
+++ src/public/js/video.js
@@ -0,0 +1,44 @@
+const socket = io();
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function handleMuteClick() {
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+function handleCameraClick() {
+  if (cameraOff) {
+    cameraBtn.innerText = "Trun Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Trun Camera On";
+    cameraOff = true;
+  }
+}
+
+mute.addEventListener("click", handleMuteClick);
+camera.addEventListener("click", handleCameraClick);
+
+// getMedia();
--- src/server.js
+++ src/server.js
@@ -10,9 +10,8 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
+app.get("/video", (_, res) => res.render("video"));
 app.get("/*", (_, res) => res.redirect("/"));
-
-const handleListen = () => console.log("Listening on http://localhost:3000");
 
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
@@ -26,77 +25,5 @@ instrument(io, {
   auth: false,
 });
 
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return io.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-io.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done(countRoom(roomName));
-    io.sockets.emit("change_room", publicRooms());
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-  });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => {
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
-    });
-  });
-  socket.on("disconnect", () => {
-    io.sockets.emit("change_room", publicRooms());
-  });
-  socket.on("new_message", (message, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${message}`);
-    done();
-  });
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-/*
-const wss = new WebSocket.Server({ server });
-
-function onSocketClose() {
-  console.log("Disconnected from Server ❌");
-}
-let sockets = [];
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser ✅");
-  socket.on("close", onSocketClose);
-  socket.on("message", (msg) => {
-    const translatedMessageData = msg.toString("utf8");
-    const message = JSON.parse(translatedMessageData);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-  });
-  socket.send("hello");
-});
-*/
-
+const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
