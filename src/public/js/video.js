49c235ccd62ca6c433c17fcf281b15e053397b7d
bellsilver7 <bellsilver7@gmail.com>
2022-07-29 08:01:53 +0900

    카메라 스위치 기능 추가

--- src/public/js/video.js
+++ src/public/js/video.js
@@ -12,10 +12,14 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (error) {
@@ -23,13 +27,20 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstrains = {
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    );
     myFace.srcObject = myStream;
+    if (!deviceId) await getCameras();
   } catch (error) {
     console.log(error);
   }
@@ -61,8 +72,12 @@ function handleCameraClick() {
   }
 }
 
+function handleCameraChange() {
+  getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("inupt", handleCameraChange);
 
-// getMedia();
-getCameras();
+getMedia();
